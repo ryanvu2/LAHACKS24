da5655531c2aef479ef4eab26f052fea22c7c108
tomohto <toh2737@gmail.com>
2024-04-19 21:34:00 -0700

    Mongoose and Mongo Initialization

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@
 //where we register the express app
 require('dotenv').config()
 const express = require('express')
+const mongoose = require('mongoose')
 const userRoutes = require('./routes/users')
 
 //express app
@@ -17,7 +18,14 @@ app.use((req,res,next)=>{
 //routes
 app.use('/api/users',userRoutes)
 
-//listen for requests
-app.listen(process.env.PORT, ()=>{
-    console.log("listening on port " + process.env.PORT)
-})
+//connect to db
+mongoose.connect(process.env.MONGO_URI)
+    .then(()=>{
+        //listen for requests
+        app.listen(process.env.PORT, ()=>{
+            console.log("listening on port " + process.env.PORT)
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
