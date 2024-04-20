d19c5660ac48c182e2df25b212344748716834b4
ryanvu2 <ryanvuemail@gmail.com>
2024-04-20 00:44:45 -0700

    User Home Page w/ useState Bug

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import './App.css';
+import HomePage from './my-app/src/pages/user/HomePage';
 import SignIn from './my-app/src/pages/SignIn';
+
+
 
 function App() {
   return (
