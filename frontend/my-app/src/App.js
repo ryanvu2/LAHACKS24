e8f6a6736b0e499a1e6ab0d9a08d8b6441c00377
ryanvu2 <ryanvuemail@gmail.com>
2024-04-20 15:02:22 -0700

    Chart Page

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -4,13 +4,15 @@ import DocHome from './pages/doctor/HomePage'
 import SignInPage from './pages/SignIn'
 import MyCalendar from './pages/user/HomePage'
 import ThoughtBubble from './pages/user/Thoughts'
+import Charts from './pages/doctor/Charts'
 
 
 function App() {
   return (
     // <MyCalendar/>
     // <ThoughtBubble/>
-    <DocHome/>
+    // <DocHome/>
+    <Charts/>
   );
 }
 
