62f2c7db68fe06506c11e2253891347a72a686cb
tomohto <toh2737@gmail.com>
2024-04-19 20:32:07 -0700

    Backend server init

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,23 @@
 //entry file for the backend app
 //where we register the express app
-
+require('dotenv').config()
 const express = require('express')
 
 //express app
 const app = express()
 
+//middleware
+app.use((req,res,next)=>{
+    console.log(req.path, req.method)
+    next()
+})
+//route handeler to react to requests
+//routes
+app.get('/', (req,res)=>{
+    res.json({mssg:"welcome to the app"})
+})
+
 //listen for requests
-app.listen(3000, ()=>{
-    console.log("listening on port 3000")
+app.listen(process.env.PORT, ()=>{
+    console.log("listening on port " + process.env.PORT)
 })
